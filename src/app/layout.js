7133386353a06868cde1e8a6936b790b136f24dd
madnywebtech786 <madnywebtech@gmail.com>
2025-11-25 14:42:15 -0700

    Added Google verification file

--- src/app/layout.js
+++ src/app/layout.js
@@ -82,17 +82,32 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KK32N6HP');
-          `}
-        </Script>
-        {/* End Google Tag Manager */}
+       {/* Google Tag Manager */}
+  <Script id="gtm-script" strategy="afterInteractive">
+    {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-KK32N6HP');
+    `}
+  </Script>
+  {/* End Google Tag Manager */}
+
+  {/* Google Analytics (gtag.js) */}
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-2LCP9SHQQR"
+    strategy="afterInteractive"
+  />
+
+  <Script id="ga4-script" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-2LCP9SHQQR');
+    `}
+  </Script>
       </head>
 
       <body
